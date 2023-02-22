60596fbb4d2cdbe629e3a52274424829a9d05804
Davidgu21 <dagu21@student.bth.se>
2023-02-22 15:10:42 +0100

    Waiting connection!

--- Server/main.py
+++ Server/main.py
@@ -1,20 +1,34 @@
+
+## MEMES
+# #(╯°□°)╯︵ ┻━┻ :^) ┬─┬ノ( º _ ºノ)
+# (╯°Д°)╯︵/(.□ . \)
+#HAHAHAHAHHA
+# ( ͡° ͜ʖ ͡°)
+#
+## THE VOICES
+#Wake up
+#  Local imports
+from Hardware.EEG import get_EEG_data
+from Hardware.Eyetracker.eyetracker import EyeTracker
+
+# imports
 import pandas as pd
 import threading
 import time
 import datetime
 import socket
 
+
 HOST = "127.0.0.1"
-PORT = 6969
+PORT = 6969 #lustigt. najs.
 extension_connected = False
 
-eeg_data = {
-    "timedate": [datetime.datetime.now()],
-    "eeg_beta": ["14"],
-    "eeg_alpha": ["8"],
-    "eeg_theta": ["5"]
-    }
+eeg_data_dict = {}
 
+
+full_df = pd.DataFrame()
+eeg_data_dict = {}
+eye_data_dict = {}
 
 #extension settings
 settings = {
@@ -22,6 +36,7 @@ settings = {
     "EEG": True,
     "E4": False
     }
+
 
 
 #handles the connection to the extension
@@ -39,36 +54,43 @@ def extension_connection():
             data = conn.recv(1024)
             if not data:
                 break
-
-def analyze_EEG():
-    print("analyzing EEG data.")
+ 
+## 
+def get_EEG_data():
+    global eeg_data_dict
     while True:
-        df = pd.DataFrame(eeg_data)
-        print(df.tail(1))
+        eeg_data_dict = get_EEG_data.get_EEG_data()
+
+def get_eye_tracker_data():
+    eye_tracker = EyeTracker(1)
+    eye_tracker.setup()
+    # eye_tracker.start_recording() 
+    
+    global eye_data_dict
+    
+    eye_tracker.start_recording(eye_data_dict)
+    
+    # while True:
+    #     eye_tracker.store_to_cache(eye_tracker.eye_tracker.get_gaze_position())
+    #     eye_data_dict = eye_tracker.get_recording()
+        
+
+def update_dataframe():
+    global full_df
+    global eye_data_dict
+    global eeg_data_dict
+
+    while True:
         time.sleep(1)
+        full_data_dic = {}
+        full_data_dic.update(eye_data_dict)
+        full_data_dic.update(eeg_data_dict)
+        
+        full_df.append(full_data_dic)
 
-def eeg_connection():
-    #connect to EEG & read data
-    while True:
-        time.sleep(2)
-        print("reading EEG")
-        
-def empaticaE4_connection():
-    #connect to E4 watch & read data
-    while True:
-        time.sleep(2)
-        print("reading E4")
 
-        
-def eyetracker_connection():
-    #connect to eyetracker watch & read data
-    while True:
-        time.sleep(2)
-        print("reading eyetracker")
-        
 
 if __name__ == "__main__":
-    
     extensionCon_thread = threading.Thread(target=extension_connected, daemon=True)
     extensionCon_thread.start()
 
@@ -78,12 +100,19 @@ if __name__ == "__main__":
         print("waiting for connection")
     print("extension connected")
     
-    
-    
     if settings["EEG"] == True:
-        EEG_thread = threading.Thread(target=eeg_connection, daemon=True)
-        analyzeEEG = threading.Thread(target=analyze_EEG, daemon=True)
-        EEG_thread.start()
-        analyzeEEG.start()
-        
+        #start thread/-s needed for EEG
+        eeg_thread = threading.Thread(target=get_EEG_data, daemon=True)
+        eeg_thread.start()
     
+    if settings["Eye tracker"] == True:
+        #start thread/-s needed for EEG
+        eye_thread = threading.Thread(target=get_EEG_data, daemon=True)
+        eye_thread.start()
+
+        
+    # once every second time values are stored                                  ####### ADDERA LOOP
+    df_thread = threading.Thread(target=update_dataframe, daemon=True)
+    df_thread.start()
+    
+
