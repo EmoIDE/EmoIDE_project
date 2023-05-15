f19bd0dfbac80cfe1f0d5dd4270d7c16935c5b41
Emil Hansson <89981140+Hnsson@users.noreply.github.com>
2023-05-15 20:33:46 +0200

    Add files via upload
    Docstrings added to the class in its functions

--- Server/Hardware/Eyetracker/eyetracker.py
+++ Server/Hardware/Eyetracker/eyetracker.py
@@ -2,7 +2,20 @@ from .gazepoint import gazepoint
 import time
 
 class EyeTracker:
+    """
+    EyeTracker class representing an EyeTracker object that connects to the GazePoint Eyetracker and retrieves data.
+    """
+
     def __init__(self, frequency, recording_length):
+        """
+        Initializes an EyeTracker instance.
+
+        Also initializes the locations of the different zones on the monitor.
+
+        Args:
+            frequency (int): The frequency at which gaze data is recorded.
+            recording_length (int): The length of the recording in seconds.
+        """
         self.recording_length = recording_length
         self.eye_tracker = None
         self.recording = False
@@ -20,7 +33,19 @@ class EyeTracker:
         ]
 
     def setup(self):
-        # Initializes default values and start calibration for use, may take 10 seconds
+        """
+        Initializes the eye tracker and performs calibration.
+
+        Returns:
+            None
+
+        Raises:
+            Exception: If there is an error setting up the eye tracker.
+
+        Notes:
+            -   The setup of the eyetracker object may take up to 100 seconds.
+        """
+
         try:
             self.eye_tracker = gazepoint.GazePoint()
         except:
@@ -28,6 +53,23 @@ class EyeTracker:
         return
 
     def check(self, value, value_range):
+        """
+        Checks if a value falls within a given range.
+
+        Args:
+            value (float): The value to check.
+            value_range (tuple): The range to compare against (start, end).
+
+        Returns:
+            bool: True if the value falls within the range, False otherwise.
+
+        Raises:
+            Exception: If there is an error during the check.
+        
+        Examples:
+            check(0.5, (0.2, 1.0))
+            # Output: True
+        """
         try:
             if value_range[0] <= value <= value_range[1]:
                 return True
@@ -36,6 +78,18 @@ class EyeTracker:
         return False
     
     def get_zone_destribution(self):
+        """
+        Calculates the distribution of gaze zones.
+
+        Take in the current count for each zones and creates a dictionary that sums up
+        the destribution in percentages for each zone.
+
+        Returns:
+            dict: A dictionary containing the zone distribution.
+
+        Notes:
+            -   The zone distribution is calculated based on the counts of each zone relative to the total count.
+        """
         zone_destribution = {}
         for zone in self.zones:
             if sum([z['count'] for z in self.zones]) != 0:
@@ -44,21 +98,65 @@ class EyeTracker:
         return zone_destribution
 
     def start_recording(self, dict, session_status):
-        # Start recording data to cache
+        """
+        Starts recording gaze data to the cache.
+
+        Constantly at a frequency call function 'store_to_cache()' with the current coordinates of the gaze
+        and also direct the 'dict' argument reference to be updated.
+
+        Args:
+            dict (dict): A dictionary to store the recorded data.
+            session_status (bool): Flag indicating if the recording session is active.
+
+        Notes:
+            -   The recorded gaze data is stored in the cache at a specified frequency.
+        """
+
         while session_status:
             self.store_to_cache(self.eye_tracker.get_gaze_position(), dict)
             time.sleep(self.frequency)
 
     def get_recording(self):
-        # Return cache with most recent coordinate
+        """
+        Retrieves the recorded gaze data from the cache.
+
+        Returns:
+            dict: The recorded gaze data.
+
+        Notes:
+            The returned data is the most recent coordinate stored in the cache.
+        """
+
         return self.cache
 
     def stop_recording(self):
-        # Stop recording data and return most recent coordinate
+        """
+        Stops the recording and returns the recorded gaze data.
+
+        Returns:
+            dict: The recorded gaze data.
+
+        Notes:
+            This function also stops the recording process.
+        """
+
         self.recording = False
         return self.get_recording()
 
     def store_to_cache(self, coordinate, dict):
+        """
+        Stores a given coordinate to the cache.
+
+        This function update the dictionary 'dict' with the current coordinate. Also mark which zone
+        the coordinate was located in and then update the zone destribution accordingly.
+
+        Args:
+            coordinate (tuple): The coordinate to store (x, y).
+            dict (dict): The dictionary to store the coordinate.
+
+        Raises:
+            Exception: If any exception occurs during the rounding and sum of zone counts.
+        """
         # Store given coordinate to cahce
         dict["x"] = coordinate[0]
         self.cache["x"].append(coordinate[0])
@@ -85,15 +183,18 @@ class EyeTracker:
                 print("[ERROR] zone = 0")
 
     def stop(self):
-        # Clear cache, stop eyetracker may take 5 seconds
+        """
+        Clears the cache and stops the eye tracker.
+        """
+
         self.cache.clear()
         self.eye_tracker.stop()
 
 
-if __name__ == "__main__":
-    eyetracker = EyeTracker()
-    eyetracker.setup()
-    eyetracker.start_recording()
-    time.sleep(10)
-    print(eyetracker.get_recording())
-    eyetracker.stop()
+# if __name__ == "__main__":
+#     eyetracker = EyeTracker()
+#     eyetracker.setup()
+#     eyetracker.start_recording()
+#     time.sleep(10)
+#     print(eyetracker.get_recording())
+#     eyetracker.stop()
